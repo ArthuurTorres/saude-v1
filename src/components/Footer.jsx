ba020672abe61113d0d53293f5b7143ca2287960
Arthur Torres <arthur-2008@hotmail.com>
2023-05-31 21:41:19 -0300

    add responsive v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,14 @@ import linkedin from '../assets/linkedin.svg'
 export const Footer = () => {
   return (
       <footer>
+
         <div>
           <img className='logo-footer' src={logof} alt="Logo Footer"/>
         </div>
+
         <div className='contact'>
           <h2>Contato</h2>
-          <ul className='footer-contact'> 
+          <ul> 
             <a href="#">
               (22) 9999-9999
               </a>
@@ -21,7 +23,11 @@ export const Footer = () => {
               email@email.com
               </a>
           </ul>
-          <ul className='footer-icons'>
+        </div>
+
+        <div className='footer-icons'>
+          <h2>Siga-nos:</h2>
+          <ul>
             <a href='#'>
               <img src={facebook} alt="facebook"/>
             </a>
@@ -36,23 +42,19 @@ export const Footer = () => {
             </a>
           </ul>
         </div>
-        <div className='contact'>
+
+        <div className='footer-information'>
           <h2>Informações</h2>
-          <ul className='footer-contact'>
-          <p>Consulte as regras, planos elegíveis e quando usar o Médico na Tela
-             em https://saudenatela.sulamerica.com.br/. Esse serviço não é indicado
-              para casos de emergência médica. Este material contém informações
-               resumidas que poderão sofrer alteração sem aviso prévio. Os benefícios
-                aqui descritos poderão ser descontinuados ou alterados a exclusivo
-                 critério da SulAmérica. Existem limitações de especialidades para
-                  os planos SulAmérica Direto. O uso dos canais de atendimento
-                   estão sujeitos à capacidade técnica e são temporários, podendo
-                    ser descontinuados a qualquer momento. O produto SulAmérica Saúde obedece à legislação
-                     e às condições contratuais, que devem ser lidas previamente à contratação. Sul América
-                      Companhia de Seguro Saúde - CNPJ: 01.685.053/0013-90 e Sul América Serviços de Saúde S.A. - 
-                      CNPJ: 02.866.602/0001- 51. SAC: 0800-722-0504 e Ouvidoria: 0800 725 3374, 
-                      horário atendimento 08:30h às 17:30h.
-          </p>
+          <ul>
+          <a href='#'>
+            Quem Somos
+          </a>
+          <a href='#'>
+            Beneficios
+          </a>
+          <a href='#'>
+            Unidades
+          </a>
          </ul>
         </div>
       </footer>
